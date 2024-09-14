093aea24e437a54f0d8c31d9e0f0da0d0e614e9d
Mohamed Aglan <mohamedaglan.dev@gmail.com>
2024-09-14 13:03:56 +0300

    Update Package.swift and dependencies for version 1.0.1

--- Package.swift
+++ Package.swift
@@ -11,25 +11,20 @@ let package = Package(
         .iOS(.v12)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "GAppUtility",
             targets: ["GAppUtility"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/mohamedaglan2225/GAppSettings", .revision("014a472bfdfc4fd7497953575e8d036099ef5e5e"))
+        .package(url: "https://github.com/mohamedaglan2225/GAppSettings.git", from: "1.0.1")
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "GAppUtility",
-            dependencies: ["GAppSettings"]),
+            dependencies: ["GAppSettings"]
+        ),
         .testTarget(
             name: "GAppUtilityTests",
             dependencies: ["GAppUtility"]),
     ]
 )
-
-
